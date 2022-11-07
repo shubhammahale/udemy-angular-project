4fe867a930af572c057027b6a645dd510dba4766
Mahale <smahale@netapp.com>
2022-11-07 16:02:18 +0530

    add http methods with firebase backend to store and fetch recipes

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -8,22 +8,29 @@ import { Recipe } from './recipe.model';
 export class RecipeService {
   recipesChanged = new Subject<Recipe[]>();
 
-  private recipes: Recipe[] = [
-    new Recipe(
-      'A Test Recipe',
-      'This is simply a test',
-      'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg',
-      [new Ingredient('Meat', 1), new Ingredient('French Fries', 20)]
-    ),
-    new Recipe(
-      'A Test Recipe 2',
-      'This is simply a test 2',
-      'https://m.timesofindia.com/thumb/msid-87930581,width-1200,height-900,resizemode-4/.jpg',
-      [new Ingredient('Buns', 2), new Ingredient('Meat', 1)]
-    ),
-  ];
+  // private recipes: Recipe[] = [
+  //   new Recipe(
+  //     'A Test Recipe',
+  //     'This is simply a test',
+  //     'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg',
+  //     [new Ingredient('Meat', 1), new Ingredient('French Fries', 20)]
+  //   ),
+  //   new Recipe(
+  //     'A Test Recipe 2',
+  //     'This is simply a test 2',
+  //     'https://m.timesofindia.com/thumb/msid-87930581,width-1200,height-900,resizemode-4/.jpg',
+  //     [new Ingredient('Buns', 2), new Ingredient('Meat', 1)]
+  //   ),
+  // ];
+  private recipes: Recipe[] = [];
 
   constructor(private shoppingListService: ShoppingListService) {}
+
+  setRecipes(recipes: Recipe[]) {
+    console.log(recipes['recipes']);
+    this.recipes = recipes;
+    this.recipesChanged.next(this.recipes);
+  }
 
   getRecipes() {
     return this.recipes.slice();
